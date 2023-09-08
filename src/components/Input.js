22f5485dcf4186517bbe631ecdef6d3d5ab93645
BrunaCardoso7 <obitadrawing@gmail.com>
2023-09-08 16:13:22 -0300

    finalizado verificador de senha

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,31 +5,33 @@ import { ValPassword } from "./style/ValPassword";
 function Input() {
     
     const [value, setValue] = useState('');
-    const [isButotonDisabled, setButtonDisabled] = useState(true)
-
-    const validationLength = value.length >=8 ? { color: 'green' } : { color: 'red' };
-    
-    const reg = /[A-Z]/
-    const validationUpper = reg.test(value)? {color: 'green'}: {color: 'red'};
-    
-    const regex = /[0-9]/
-    const validationNuber = regex.test(value)? {color: 'green'} : {color: 'red'}
+    const [isButotonDisabled, setButtonDisabled] = useState(true);    
     
     const handleChecked = (evt)=>{
-        setValue(evt.target.value)
+        const inputValue = evt.target.value
+        setValue(inputValue)
         
+        const validationLength = value.length >=8;
+
+        const validationUpper = /[A-Z]/.test(inputValue);
+
+        const validationNumber = /[0-9]/.test(inputValue);
+
+        setButtonDisabled(!validationLength || !validationNumber || !validationUpper)
     }
-
-
-
+    const handleSubmit = (evt)=>{
+        evt.preventDefault()
+        alert("senha salva com sucesso cidadão!")
+        window.location.reload()
+    }
     return ( 
     <>
-        <form action="#">
+        <form onSubmit={ handleSubmit }>
             <Conteiner>
                 <p>Sua senha precisa ter:</p>
-                <ValPassword style={validationNuber}>✅No minimo 1 número</ValPassword>
-                <ValPassword style={validationUpper}>✅Pelo menos uma letra Maiúscula</ValPassword>
-                <ValPassword style={validationLength}>✅No minímo 8 caracteres</ValPassword>
+                <ValPassword style={{ color: /[0-9]/.test(value)? 'green': 'red' }}>✅No minimo 1 número</ValPassword>
+                <ValPassword style={{ color: /[A-Z]/.test(value)? 'green':'red' }}>✅Pelo menos uma letra Maiúscula</ValPassword>
+                <ValPassword style={{ color: value.length >=8? 'green':'red' }}>✅No minímo 8 caracteres</ValPassword>
                 <input type="text" value={ value } onChange={ handleChecked } />
                 <button type="submit" disabled={isButotonDisabled}>salva</button>
             </Conteiner>
